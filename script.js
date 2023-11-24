6601bd60513f289995126408b4e2d26bd9d9d9bc
Silvan Stuber <silvan.stuber93@gmail.com>
2023-11-24 19:23:32 +0100

    import image from API

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentPokemon;
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    let url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
     let response = await fetch(url);
     currentPokemon = await response.json();
 
@@ -11,4 +11,5 @@ async function loadPokemon() {
 
 function renderPokemonInfo() {
     document.getElementById('pokemonName').innerHTML = currentPokemon['name']
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_shiny'];
 }
