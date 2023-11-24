8340d569e85eac1e7691ffd40a08c52942c78aab
Silvan Stuber <silvan.stuber93@gmail.com>
2023-11-24 20:53:15 +0100

    accessed the svg + positioning of the image

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentPokemon;
 
 async function loadPokemon() {
-    let url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
+    let url = `https://pokeapi.co/api/v2/pokemon/1/`; 
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log('Loaded loadPokemon', currentPokemon)
@@ -10,10 +10,10 @@ async function loadPokemon() {
 
 function renderPokemonInfo() {
     document.getElementById('pokemonName').innerHTML = generateCapitalizeNameOfPokemon();
-    document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_shiny'];
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['dream_world']['front_default'];
 }
 
 function generateCapitalizeNameOfPokemon() {
-    let currentPokemonName = currentPokemon['name'];
-    return currentPokemonName.charAt(0).toUpperCase() + currentPokemonName.slice(1);
+    let currentPokemonName = currentPokemon['name']; //reads the name of the Pokemon from the JSON
+    return currentPokemonName.charAt(0).toUpperCase() + currentPokemonName.slice(1); //since everything is extracted from the JSON as lowercase letters, we use this function to capitalise the first letter
 }
