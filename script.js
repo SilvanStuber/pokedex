cc38a856c35e53152b3d5f9d128c74c00dbb5b64
Silvan Stuber <silvan.stuber93@gmail.com>
2023-11-24 19:47:28 +0100

    function for capitalising the first letter of the Pokemon's name

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ async function loadPokemon() {
     let url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
     let response = await fetch(url);
     currentPokemon = await response.json();
-
     console.log('Loaded loadPokemon', currentPokemon)
     renderPokemonInfo();
 }
 
 function renderPokemonInfo() {
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name']
+    document.getElementById('pokemonName').innerHTML = generateCapitalizeNameOfPokemon();
     document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_shiny'];
+}
+
+function generateCapitalizeNameOfPokemon() {
+    let currentPokemonName = currentPokemon['name'];
+    return currentPokemonName.charAt(0).toUpperCase() + currentPokemonName.slice(1);
 }
