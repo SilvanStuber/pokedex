f9e5dc92390156eb839f351e0bda7a986ba2469c
Silvan Stuber <silvan.stuber93@gmail.com>
2023-11-25 18:42:29 +0100

    id of pokemon integrated and map designed

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ async function loadPokemon() {
     let url = `https://pokeapi.co/api/v2/pokemon/1/`; 
     let response = await fetch(url);
     currentPokemon = await response.json();
+    let nameFromPokemon = currentPokemon['name'];
     console.log('Loaded loadPokemon', currentPokemon)
+    console.log(nameFromPokemon)
     renderPokemonInfo();
 }
 
 function renderPokemonInfo() {
     document.getElementById('pokemonName').innerHTML = generateCapitalizeNameOfPokemon();
     document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['dream_world']['front_default'];
+    document.getElementById('idFromPokemon').innerHTML = `#${currentPokemon['id']}`;
 }
 
 function generateCapitalizeNameOfPokemon() {
