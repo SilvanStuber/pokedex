6e5b359eca9280c05fc2c0ccc8c5c06bb2d7a709
Silvan Stuber <silvan.stuber93@gmail.com>
2023-11-26 16:45:15 +0100

    type of the Pokemon

---
+++ js/data.js
@@ -0,0 +1,151 @@
+let currentPokemon;
+let nameFromPokemon;
+let specificationsOfThePokemon;
+let nameFromPokemonGerman;
+let backgroundColor;
+let generaOfThePokemonInGerman;
+let heightFromPokemonMeter;
+let heightFromPokemonootFoot;
+let weightFromPokemonKilogram;
+let weightFromPokemonootPound;
+let typeFromPokemonInGerman = [];
+
+function generateBackgroundColor() {
+  backgroundColor = specificationsOfThePokemon["color"]["name"];
+  console.log();
+  if (backgroundColor === "white") {
+    return `background-color: rgb(165, 196, 243);`;
+  }
+  if (backgroundColor === "red") {
+    return `background-color: rgb(251,108,108);`;
+  }
+  if (backgroundColor === "green") {
+    return `background-color: rgb(72,207,177);`;
+  }
+  if (backgroundColor === "blue") {
+    return `background-color: rgb(118,189,254);`;
+  }
+  if (backgroundColor === "brown") {
+    return `background-color: rgb(137,80,48);`;
+  }
+  if (backgroundColor === "yellow") {
+    return `background-color: rgb(255,216,111);`;
+  }
+  if (backgroundColor === "gray") {
+    return `background-color: rgb(102, 103, 106);`;
+  }
+  if (backgroundColor === "purple") {
+    return `background-color: rgb(65,5,114);`;
+  }
+  if (backgroundColor === "pink") {
+    return `background-color: rgb(247,102,173);`;
+  }
+  if (backgroundColor === "black") {
+    return `background-color: rgb(0,0,0);`;
+  } else {
+    console.log(`${backgroundColor} does not exist`);
+  }
+}
+
+function generateTypeInGerman() {
+    typeFromPokemonInGerman = [];
+  for (let i = 0; i < currentPokemon["types"].length; i++) {
+    const typeFromPokemon = currentPokemon["types"][i]["type"]["name"];
+    if (typeFromPokemon === "normal") {
+      typeFromPokemonInGerman.push("Normal");
+    }
+    if (typeFromPokemon === "fighting") {
+      typeFromPokemonInGerman.push("Kampf");
+    }
+    if (typeFromPokemon === "flying") {
+      typeFromPokemonInGerman.push("Flug");
+    }
+    if (typeFromPokemon === "poison") {
+      typeFromPokemonInGerman.push("Gift");
+    }
+    if (typeFromPokemon === "ground") {
+      typeFromPokemonInGerman.push("Boden");
+    }
+    if (typeFromPokemon === "rock") {
+      typeFromPokemonInGerman.push("Gestein");
+    }
+    if (typeFromPokemon === "bug") {
+      typeFromPokemonInGerman.push("Käfer");
+    }
+    if (typeFromPokemon === "ghost") {
+      typeFromPokemonInGerman.push("Geist");
+    }
+    if (typeFromPokemon === "steel") {
+      typeFromPokemonInGerman.push("Stahl");
+    }
+    if (typeFromPokemon === "fire") {
+      typeFromPokemonInGerman.push("Feuer");
+    }
+    if (typeFromPokemon === "water") {
+      typeFromPokemonInGerman.push("Wasser");
+    }
+    if (typeFromPokemon === "grass") {
+      typeFromPokemonInGerman.push("Pflanze");
+    }
+    if (typeFromPokemon === "electric") {
+      typeFromPokemonInGerman.push("Elektro");
+    }
+    if (typeFromPokemon === "psychic") {
+      typeFromPokemonInGerman.push("Psycho");
+    }
+    if (typeFromPokemon === "ice") {
+      typeFromPokemonInGerman.push("Eis");
+    }
+    if (typeFromPokemon === "dragon") {
+      typeFromPokemonInGerman.push("Drache");
+    }
+    if (typeFromPokemon === "dark") {
+      typeFromPokemonInGerman.push("Unlicht");
+    }
+    if (typeFromPokemon === "fairy") {
+      typeFromPokemonInGerman.push("Fee");
+    }
+    if (typeFromPokemon === "unknown") {
+      typeFromPokemonInGerman.push("Unbekannt");
+    }
+    if (typeFromPokemon === "shadow") {
+      typeFromPokemonInGerman.push("Schatten");
+    } else {
+      console.log(`${typeFromPokemon} does not exist`);
+    }
+  }
+}
+
+function generateHabitatInGerman() {
+  habitatFromPokemon = specificationsOfThePokemon["habitat"]["name"];
+  console.log(habitatFromPokemon);
+  if (habitatFromPokemon === "cave") {
+    return `Höhle`;
+  }
+  if (habitatFromPokemon === "forest") {
+    return `Wald`;
+  }
+  if (habitatFromPokemon === "grassland") {
+    return `Wiese`;
+  }
+  if (habitatFromPokemon === "mountain") {
+    return `Berge`;
+  }
+  if (habitatFromPokemon === "rare") {
+    return `selten`;
+  }
+  if (habitatFromPokemon === "rough-terrain") {
+    return `unebenes Gebiet`;
+  }
+  if (habitatFromPokemon === "sea") {
+    return `Meer`;
+  }
+  if (habitatFromPokemon === "urban") {
+    return `Stadt`;
+  }
+  if (habitatFromPokemon === "waters-edge") {
+    return `Gewässerrand`;
+  } else {
+    console.log(`${habitatFromPokemon} does not exist`);
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+
+async function loadPokemon() {
+  let url = `https://pokeapi.co/api/v2/pokemon/4/`;
+  currentPokemon = await generateJSON(url);
+  generateNameFromPokemon();
+  let urlFromSpecies = `https://pokeapi.co/api/v2/pokemon-species/${nameFromPokemon}`;
+  specificationsOfThePokemon = await generateJSON(urlFromSpecies);
+  generateNameOfPokemonInGerman();
+  generateGeneraOfThePokemonInGerman();
+  generateHeightOfTHePokemon();
+  generateWeightOfTHePokemon();
+  generateTypeInGerman();
+  console.log("Loaded loadPokemon", currentPokemon);
+  console.log(weightFromPokemonKilogram);
+  console.log(specificationsOfThePokemon);
+  console.log(weightFromPokemonootPound);
+  renderPokemonInfo();
+}
+
+function renderPokemonInfo() {
+  document.getElementById("pokemonName").innerHTML = nameFromPokemonGerman;
+  document.getElementById("pokemonImage").src = currentPokemon["sprites"]["other"]["dream_world"]["front_default"];
+  document.getElementById("idFromPokemon").innerHTML = `#${currentPokemon["id"]}`;
+  document.getElementById("headContainer").style = generateBackgroundColor();
+  document.getElementById("genraOfPokemon").innerHTML = generaOfThePokemonInGerman;
+  document.getElementById("heightMeter").innerHTML = `${heightFromPokemonMeter} m`;
+  document.getElementById("heightFoot").innerHTML = `${heightFromPokemonootFoot} ft`; 
+  document.getElementById("weightKilogram").innerHTML = `${weightFromPokemonKilogram} kg`;
+  document.getElementById("weightPound").innerHTML = `${weightFromPokemonootPound} lbs`;
+  document.getElementById("habitatPokemon").innerHTML = generateHabitatInGerman();
+  document.getElementById("typePokemon").innerHTML += ``;
+  for (let i = 0; i < typeFromPokemonInGerman.length; i++) {
+    let type = typeFromPokemonInGerman[i];
+    document.getElementById("typePokemon").innerHTML += ` <span>${type}</span>`;
+  }
+  
+   
+}
+
+function generateNameFromPokemon() {
+  nameFromPokemon = currentPokemon["name"];
+}
+
+function generateNameOfPokemonInGerman() {
+  nameFromPokemonGerman = specificationsOfThePokemon["names"]["5"]["name"];
+}
+
+
+
+function generateGeneraOfThePokemonInGerman() {
+  generaOfThePokemonInGerman = specificationsOfThePokemon["genera"]["4"]["genus"];
+}
+
+function generateHeightOfTHePokemon() {
+  heightFromPokemonMeter = currentPokemon["height"] / 10;
+  heightFromPokemonootFoot = (heightFromPokemonMeter * 3.281).toFixed(1);
+}
+
+function generateWeightOfTHePokemon() {
+  weightFromPokemonKilogram = currentPokemon["weight"] / 10;
+  weightFromPokemonootPound = (weightFromPokemonKilogram * 2.205).toFixed(1);
+}
+
+async function generateJSON(url) {
+  let response = await fetch(url);
+  return (currentJSON = await response.json());
+}
+
